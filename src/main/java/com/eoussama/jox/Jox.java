38aa5a71c4a4286522915fd4f23da475f05ee9a7
eoussama <oussama.essamadi@gmail.com>
2024-03-29 20:55:29 +0000

    entry

--- src/main/java/com/eoussama/jox/Jox.java
+++ src/main/java/com/eoussama/jox/Jox.java
@@ -1,11 +1,24 @@
 package com.eoussama.jox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 
-public class Jox 
-{
-	public static void main( String[] args )
-	{
-		System.out.println("Jox");
-	}
+
+public class Jox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jox [script]");
+      System.exit(64); 
+    } else if (args.length == 1) {
+      // runFile(args[0]);
+    } else {
+      // runPrompt();
+    }
+  }
 }
