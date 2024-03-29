37d26d9401a9816c7d6d3a8ae35851f279593d11
eoussama <oussama.essamadi@gmail.com>
2024-03-29 20:53:26 +0000

    mvn init

---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp target/jox-1.0-SNAPSHOT.jar com.eoussama.jox.Jox
---
+++ src/main/java/com/eoussama/jox/Jox.java
@@ -0,0 +1,11 @@
+package com.eoussama.jox;
+
+
+
+public class Jox 
+{
+	public static void main( String[] args )
+	{
+		System.out.println("Jox");
+	}
+}
